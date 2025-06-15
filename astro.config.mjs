594cedf8ea7483c125b57ad9f5810d0a5fc38803
picuu <dev@carloscapo.com>
2025-06-15 21:21:32 +0200

    add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config'
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({ context: 'server', access: 'public', default: true }),
       SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public', url: true })
     }
-  }
+  },
+
+  adapter: vercel()
 })
