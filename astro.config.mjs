7bd04f3fff4712bdcc2b9f3e5fcc0c74b04fdf75
picuu <carloscapoforcada@gmail.com>
2025-06-15 21:07:12 +0200

    add typing for env file and update Astro output to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({ context: 'server', access: 'public', default: true }),
+      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public', url: true })
+    }
+  }
+})
